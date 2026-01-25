9dc95324c441c6e8c56e731f64d77936e47de28f
Federico Mereu <federico.mereu@outlook.it>
2026-01-25 15:38:36 +0100

    Boh 2

---
+++ ios/Real Amis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real Amis
+//
+//  Created by Federico Mereu on 25/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Real Amis/Real_AmisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_AmisApp.swift
+//  Real Amis
+//
+//  Created by Federico Mereu on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Real_AmisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
