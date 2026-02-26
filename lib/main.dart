e6379125977b1f272b5c23cf23de663d620ff59c
Federico Mereu <federico.mereu@outlook.it>
2026-02-26 22:12:09 +0100

    Aggiornato TODO

--- lib/main.dart
+++ lib/main.dart
@@ -78,4 +78,6 @@ class MainApp extends ConsumerWidget {
   TODO:
   - Inviare notifiche per partita imminente
   - Notifica ad ogni evento?
+  - Persistenza login
+  - Rendere editabile tutta la pagina storia
 */
